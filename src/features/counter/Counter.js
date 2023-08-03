bf5913dea8d0f0f9f27b4a22802a9905128866b0
dpurcell <dpurcell.pd@gmail.com>
2023-08-03 16:44:14 -0400

    initial commit

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {increment, decrement} from './counterSlice';
+import styles from './Counter.module.css';
+
+export default function Counter() {
+    const count = useSelector(state => state.counter.value);
+    const dispatch = useDispatch()
+  return (
+    <div>
+        <div className={styles.row}>
+            <button
+                className={styles.button} 
+                aria-label="Increment value" 
+                onClick={() => dispatch(increment())}
+            >Increment</button>
+            <span className={styles.value}>{count}</span>
+            <button 
+                className={styles.button}
+                aria-label="Decrement value" 
+                onClick={() => dispatch(decrement())}
+            >Decrement</button>
+        </div>
+    </div>
+  );
+}
+
